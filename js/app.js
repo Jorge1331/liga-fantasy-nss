ad4d73ec212c7aab3ac2fd7c016b711a0cea90d7
Jorge1331 <jjlarag@gmail.com>
2025-08-15 21:09:49 +0200

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,5 @@
+function startApp() {
+  document.getElementById('intro').classList.add('hidden');
+  document.getElementById('mainApp').classList.remove('hidden');
+  // Aquí puedes añadir funciones de navegación, carga de clases, etc.
+}
